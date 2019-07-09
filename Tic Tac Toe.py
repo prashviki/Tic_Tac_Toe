5b45e35e1be4661511462d110b4f2f77e3669578
prashetc <42240489+prashetc@users.noreply.github.com>
2019-07-09 15:16:35 +0530

    board created

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,14 +1,4 @@
-# overview:
-#     play:
-#         create board
-#         display board
-#         start
-#         take input
-#         update board
-#         handle player
-#         check status:
-#             check rows
-#             check columns
-#             check diadonals
-#             check tie
-#         replay
+# create board
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
