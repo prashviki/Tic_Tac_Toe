a916f2cd7379e01dda912902794067079e083e21
prashetc <42240489+prashetc@users.noreply.github.com>
2019-07-09 15:15:44 +0530

    overview

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,1 +1,14 @@
-print('_test')
+# overview:
+#     play:
+#         create board
+#         display board
+#         start
+#         take input
+#         update board
+#         handle player
+#         check status:
+#             check rows
+#             check columns
+#             check diadonals
+#             check tie
+#         replay
